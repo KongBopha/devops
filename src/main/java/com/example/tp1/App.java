1f90f61497aa9393bced71aff4954beabf398ac4
kongbopha <kongbophaa@gmail.com>
2025-04-01 18:05:39 +0700

    Build demo

---
+++ src/main/java/com/example/tp1/App.java
@@ -0,0 +1,13 @@
+package com.example.tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
